0cea2a2242321785c7602cb89cd7b6f20002587e
dev-zockyyyy-nico <hndsbhuhdhs@gmail.com>
2020-07-09 23:51:18 +0200

    2 new Commands

--- Mod_Commands/mute.py
+++ Mod_Commands/mute.py
@@ -7,26 +7,34 @@ class MuteCommand(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def mute(self, ctx, args=None, *, member: discord.Member=None):
+    async def mute(self, ctx, member: discord.Member = None, *, args=None):
         mute_role = discord.utils.get(ctx.guild.roles, name='Mute')
         if ctx.message.author.guild_permissions.manage_messages:
             if not member:
+                await ctx.message.delete()
                 await ctx.send('**Bitte geb ein User an den du muten willst!**')
                 return
             
             if member == ctx.message.guild.owner:
-                await ctx.send(f'**{member.name} ist der Server Owner! Den kann ich nicht Muten!**')
+                await ctx.message.delete()
+                await ctx.send(f'_{member.name}_ **ist der Server Owner! Den kann ich nicht Muten!**')
                 return
 
             if member == ctx.message.author:
-                await ctx.send(f'**{ctx.message.author.name}Hast du greade versucht dich selber zu muten? War woll nix!**')
+                await ctx.message.delete()
+                await ctx.send(f'_{ctx.message.author.mention}_ **Hast du greade versucht dich selber zu muten? War woll nix!**')
                 return
 
             if not args:
+                await ctx.message.delete()
+                await member.add_roles(mute_role)
                 await ctx.send(f'**User** _{member.name}_ **wurde von** _{ctx.message.author.name}_ **gemuted!**')
             else:
-                await ctx.send(f'**User** _{member.name}_ **wurde von** _{ctx.message.author.name}_ **gemuted wegen** ```{args}```!')
+                await ctx.message.delete()
+                await member.add_roles(mute_role)
+                await ctx.send(f'**User** _{member.name}_ **wurde von** _{ctx.message.author.name}_ **gemuted wegen** ```{args}``` **!**')
         else:
+            await ctx.message.delete()
             no_permission = discord.Embed(title='No Permission', color=discord.Color.dark_red())
             no_permission.add_field(name='Keine Rechte', value='```manage messages```')
             no_permission.set_footer(text=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url_as(size=512)}')
---
+++ Mod_Commands/unmute.py
@@ -0,0 +1,31 @@
+import discord
+
+from discord.ext import commands
+
+class unmuteCommand(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def unmute(self, ctx, member: discord.Member = None, *):
+        mute_role = discord.utils.get(ctx.guild.roles, 'Mute')
+        if ctx.message.author.guild_permissions.manage_messages:
+            if not member:
+                await ctx.message.delete()
+                await ctx.send('**Bitte geb ein User an den du entmuten willst!**')
+                return
+            
+            if mute_role in member.roles:
+                await ctx.message.delete()
+                await member.remove_roles(mute_role)
+                await ctx.send(f'**User** _{member.name}_ **wurde von** _{ctx.message.author.name}_ **entmuted!**')
+            else:
+                await ctx.message.delete()
+                await ctx.send(f'**User** _{member.name}_ **ist nicht gemuted!**')
+
+        else:
+            await ctx.message.delete()
+            no_permission = discord.Embed(title='No Permission', color=discord.Color.dark_red())
+            no_permission.add_field(name='Keine Rechte', value='```manage messages```')
+            no_permission.set_footer(text=f'{ctx.author}', icon_url=f'{ctx.author.avatar_url_as(size=512)}')
+            await ctx.send(embed=no_permission)
